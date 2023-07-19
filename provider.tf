2ca09ad9a1eafe80b4d33df6f1226622e149d881
vintilacristian222 <vintilacristian222@gmail.com>
2023-07-19 11:26:45 +0300

    added gitlabci pipeline

--- provider.tf
+++ provider.tf
@@ -13,7 +13,7 @@ terraform {
 
 provider "hcloud" {
   # Configuration options
-  token =  # switch this your own token 
+  token = var.HCLOUD_TOKEN  # switch this your own token 
   endpoint = "https://api.hetzner.cloud/v1" # The API endpoint to use (optional, default is https://api.hetzner.cloud/v1)
   poll_interval = "500ms" # The interval to poll for resource updates (optional, default is 500ms)
 }
